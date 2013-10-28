bc04459fdb5003be0a6d601faa1981d9b7df19ca
Malathi <malu.t90@gmail.com>
2013-10-29 00:13:40 +0530

    Sample template for email

---
+++ templates/QuestionForwardedAlert.php
@@ -0,0 +1,13 @@
+Hi <?php echo $_GET['patient_name'] ?>,
+<br>
+Greetings from Chitra Hospital!!! The question you asked us has been forwarded to senior doctors for review.
+<br>
+Kindly have patience and we will get back to you as soon as possible.
+<br>
+<b>Question : </b>
+<br>
+<?php echo $_GET['question'] ?>
+<br>
+<b>Question Description: </b>
+<br>
+<?php echo $_GET['question _desc'] ?>
