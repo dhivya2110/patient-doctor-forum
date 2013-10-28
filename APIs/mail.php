e6b9d188f91f814f088a88eb648ffe676b2f287e
dhivya2110 <dhivya2110@gmail.com>
2013-10-28 23:32:39 +0530

    HTML mail

--- APIs/mail.php
+++ APIs/mail.php
@@ -1,5 +1,18 @@
 <?php 
 	include '../Utils/send_mail.php';
-	send_mail("dhivya2110@gmail.com","Test Mail","Hiiiii...");
+	$message = <<<EOF
+<html>
+  <body bgcolor="#DCEEFC">
+    <center>
+        <b>Looool!!! I am reciving HTML email......</b> <br>
+        <font color="red">Thanks Mohammed!</font> <br>
+        <a href="http://www.maaking.com/">* maaking.com</a>
+    </center>
+      <br><br>*** Now you Can send HTML Email <br>
+  </body>
+</html>
+EOF;
+	$to="malu.t90@gmail.com";
+	send_mail($to,"Test Mail",$message);
 	
 ?>
